3fd70621775575272555c3cc0e1c3a0c58009fae
Jens Hahn <jens.hahn@hu-berlin.de>
2017-11-20 15:09:12 +0100

    changed PythonTest and sol.pkl

--- PythonIntro/20171103_PythonTest.py
+++ PythonIntro/20171103_PythonTest.py
@@ -10,7 +10,7 @@ class PythonTest:
 
     def test_01(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 1:')
         print('x = 7')
         print('y = x')
         print('x = 3')
@@ -21,7 +21,7 @@ class PythonTest:
         
     def test_02(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 2:')
         print('x = 5')
         print('y = 2')
         print('z = x == y')
@@ -32,7 +32,7 @@ class PythonTest:
         
     def test_03(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 3:')
         print('x = 5')
         print('y = 2')
         print('z = x%y')
@@ -43,7 +43,7 @@ class PythonTest:
         
     def test_04(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 4:')
         print('numbers = [1,2,3,4,5,6,7,8,9]')
         print('sum = 0')
         print('for x in numbers:')
@@ -55,7 +55,7 @@ class PythonTest:
         
     def test_05(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 5:')
         print('x = 5')
         print('if x == 2:')
         print('    y = 3')
@@ -69,7 +69,7 @@ class PythonTest:
 
     def test_06(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 6:')
         print('x = 1')
         print('def my_func():')
         print('\t x = 2\n')
@@ -81,7 +81,7 @@ class PythonTest:
         
     def test_07(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 7:')
         print('x = 1')
         print('def my_func():')
         print('\t x = 2')
@@ -91,10 +91,9 @@ class PythonTest:
             answer = input('Was wird der print-Befehl ausgeben?')
         print('Richtig!\n')
 
-
     def test_08(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 8:')
         print('l = [1,2,3,4]')
         answer = None
         while answer != self.sol['08']:
@@ -103,7 +102,7 @@ class PythonTest:
 
     def test_09(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 9:')
         print('l = [1,2,3,4]')
         answer = None
         while answer != self.sol['09']:
@@ -112,7 +111,16 @@ class PythonTest:
 
     def test_10(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 9:')
+        print('l = [1,2,3,4]')
+        answer = None
+        while answer != self.sol['10']:
+            answer = input('Welche Zahl ist l[-1]?')
+        print('Richtig!\n')
+
+    def test_10(self):
+        print('--------------------------------------------')
+        print('Frage 10:')
         print('l = [1,2,3,4]')
         print('l2 = l')
         print('l.append("meep")')
@@ -123,7 +131,7 @@ class PythonTest:
 
     def test_11(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 11:')
         print('l = [1,2,3,4,5]')
         print('l.extend([6,7,8])')
         answer = None
@@ -133,7 +141,7 @@ class PythonTest:
 
     def test_12(self):
         print('--------------------------------------------')
-        print('Frage:')
+        print('Frage 12:')
         print('l = [1,2,3,4,5]')
         print('l.append([6,7,8])')
         answer = None
@@ -143,6 +151,7 @@ class PythonTest:
 
     def test_13(self):
         print('--------------------------------------------')
+        print('Frage 13:')
         print('l = [1,2,3,4]')
         print('a = l.append(5)')
         answer = None
@@ -152,20 +161,21 @@ class PythonTest:
 
     def test_14(self):
         print('--------------------------------------------')
-        print('def my_func(l=[]):')
-        print('\tl.append("item")')
+        print('Frage 14:')
+        print('def my_func(l=2):')
         print('\treturn l')
         print('a = my_func()')
-        print('b = my_func()')
+        print('b = my_func(7)')
         answer = None
         answer2 = None
         while answer != self.sol['14a']:
-            answer = input('Was ist die Länge von a?')
+            answer = input('Was ist a?')
         while answer2 != self.sol['14b']:
-            answer2 = input('Was ist die Länge von b?')
+            answer2 = input('Was ist b?')
 
     def test_15(self):
         print('--------------------------------------------')
+        print('Frage 15:')
         print('numbers = [1,2,3,4,5,6,7,8,9]')
         print('sum = 0')
         print('for x in numbers:')
@@ -177,7 +187,8 @@ class PythonTest:
 
     def test_16(self):
         print('--------------------------------------------')
-        print('x = 10')
+        print('Frage 16:')
+        print('x = 3')
         print('i = 0')
         print('while i <= x:')
         print('\ti = i + 1')
@@ -188,7 +199,8 @@ class PythonTest:
 
     def test_17(self):
         print('--------------------------------------------')
-        print('x = 10')
+        print('Frage 17:')
+        print('x = 3')
         print('i = 0')
         print('y = 0')
         print('while i <= x:')
@@ -201,7 +213,8 @@ class PythonTest:
 
     def test_18(self):
         print('--------------------------------------------')
-        print('x = 10')
+        print('Frage 18:')
+        print('x = 3')
         print('i = 0')
         print('y = 0')
         print('while i <= x:')
@@ -214,7 +227,8 @@ class PythonTest:
 
     def test_19(self):
         print('--------------------------------------------')
-        print('x = 10')
+        print('Frage 19:')
+        print('x = 3')
         print('i = 0')
         print('y = 0')
         print('while i < x:')
@@ -225,9 +239,9 @@ class PythonTest:
         while answer != self.sol['19']:
             answer = input('Was gibt der print Befehl aus?')
 
-
     def test_20(self):
         print('--------------------------------------------')
+        print('Frage 20:')
         print('x = 10')
         print('i = 0')
         print('y = 0')
@@ -246,6 +260,7 @@ class PythonTest:
 
     def test_21(self):
         print('--------------------------------------------')
+        print('Frage 21:')
         print('x = 10')
         print('i = 0')
         print('y = 0')
@@ -264,6 +279,7 @@ class PythonTest:
 
     def test_22(self):
         print('--------------------------------------------')
+        print('Frage 22:')
         print('answer = None')
         print('i = 7')
         print('while answer != 7:')
@@ -281,6 +297,7 @@ class PythonTest:
 
     def test_23(self):
         print('--------------------------------------------')
+        print('Frage 23:')
         print('def my_func(my_list):')
         print('\tmy_list.append(42)\n')
         print('other_list = []')
@@ -292,6 +309,7 @@ class PythonTest:
 
     def test_24(self):
         print('--------------------------------------------')
+        print('Frage 24:')
         print('def my_func(my_int):')
         print('\tmy_int = my_int + 1\n')
         print('other_int = 3')
@@ -301,11 +319,26 @@ class PythonTest:
         while answer != self.sol['24']:
             answer = input('Was gibt der print-Befehl aus?')
 
+    def test_25(self):
+        print('--------------------------------------------')
+        print('Frage 25:')
+        print('def my_func(my_int):')
+        print('\tmy_int = my_int + 1\n')
+        print('\treturn my_int\n')
+        print('other_int = 3')
+        print('other_int = my_func(other_int)')
+        print('print(other_int)')
+        answer = None
+        while answer != self.sol['25']:
+            answer = input('Was gibt der print-Befehl aus?')
+
+
 
 if __name__ == "__main__":
     test = PythonTest('sol.pkl')
     attrs = (getattr(test, name) for name in dir(test))
     methods = filter(inspect.ismethod, attrs)
+    methods = sorted(methods, key=lambda x: str(x))
     for method in methods:
         try:
             method()
