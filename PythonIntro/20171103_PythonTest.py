7a23ad6cb03211c72a3ec46598b3a34760c4dd30
theinfluenzor <diemerjo@hu-berlin.de>
2017-11-20 20:58:30 +0100

    all questions, with correct answer

--- PythonIntro/20171103_PythonTest.py
+++ PythonIntro/20171103_PythonTest.py
@@ -118,95 +118,81 @@ class PythonTest:
             answer = input('Welche Zahl ist l[-1]?')
         print('Richtig!\n')
 
-    def test_10(self):
+    def test_11(self):
         print('--------------------------------------------')
-        print('Frage 10:')
+        print('Frage 11:')
         print('l = [1,2,3,4]')
         print('l2 = l')
         print('l.append("meep")')
         answer = None
-        while answer != self.sol['10']:
-            answer = input('Wie viele Elemente enthält l2?')
-        print('Richtig!\n')
-
-    def test_11(self):
-        print('--------------------------------------------')
-        print('Frage 11:')
-        print('l = [1,2,3,4,5]')
-        print('l.extend([6,7,8])')
-        answer = None
         while answer != self.sol['11']:
-            answer = input('Was ist die Länge von l?')
+            answer = input('Wie viele Elemente enthält l2?')
         print('Richtig!\n')
 
     def test_12(self):
         print('--------------------------------------------')
         print('Frage 12:')
         print('l = [1,2,3,4,5]')
-        print('l.append([6,7,8])')
+        print('l.extend([6,7,8])')
         answer = None
         while answer != self.sol['12']:
             answer = input('Was ist die Länge von l?')
-        print('Richtig!\n')    
+        print('Richtig!\n')
 
     def test_13(self):
         print('--------------------------------------------')
         print('Frage 13:')
-        print('l = [1,2,3,4]')
-        print('a = l.append(5)')
+        print('l = [1,2,3,4,5]')
+        print('l.append([6,7,8])')
         answer = None
         while answer != self.sol['13']:
-            answer = input('Was ist a?')
-        print('Richtig!\n')
+            answer = input('Was ist die Länge von l?')
+        print('Richtig!\n')    
 
     def test_14(self):
         print('--------------------------------------------')
         print('Frage 14:')
+        print('l = [1,2,3,4]')
+        print('a = l.append(5)')
+        answer = None
+        while answer != self.sol['14']:
+            answer = input('Was ist a?')
+        print('Richtig!\n')
+
+    def test_15(self):
+        print('--------------------------------------------')
+        print('Frage 15:')
         print('def my_func(l=2):')
         print('\treturn l')
         print('a = my_func()')
         print('b = my_func(7)')
         answer = None
         answer2 = None
-        while answer != self.sol['14a']:
+        while answer != self.sol['15a']:
             answer = input('Was ist a?')
-        while answer2 != self.sol['14b']:
+        while answer2 != self.sol['15b']:
             answer2 = input('Was ist b?')
 
-    def test_15(self):
+    def test_16(self):
         print('--------------------------------------------')
-        print('Frage 15:')
+        print('Frage 16:')
         print('numbers = [1,2,3,4,5,6,7,8,9]')
         print('sum = 0')
         print('for x in numbers:')
         print('\tif x%2 == 0:')
         print('\t\tsum = sum + x')
         answer = None
-        while answer != self.sol['15']:
-            answer = input('Welchen Wert hat sum?')
-
-    def test_16(self):
-        print('--------------------------------------------')
-        print('Frage 16:')
-        print('x = 3')
-        print('i = 0')
-        print('while i <= x:')
-        print('\ti = i + 1')
-        print('print(i)')
-        answer = None
         while answer != self.sol['16']:
-            answer = input('Was gibt der print Befehl aus?')
+            answer = input('Welchen Wert hat sum?')
 
     def test_17(self):
         print('--------------------------------------------')
         print('Frage 17:')
         print('x = 3')
         print('i = 0')
-        print('y = 0')
         print('while i <= x:')
-        print('\ty = y + i')
         print('\ti = i + 1')
-        print('print(y)')
+        print('print(i)')
         answer = None
         while answer != self.sol['17']:
             answer = input('Was gibt der print Befehl aus?')
@@ -218,8 +204,8 @@ class PythonTest:
         print('i = 0')
         print('y = 0')
         print('while i <= x:')
-        print('\ti = i + 1')
         print('\ty = y + i')
+        print('\ti = i + 1')
         print('print(y)')
         answer = None
         while answer != self.sol['18']:
@@ -231,7 +217,7 @@ class PythonTest:
         print('x = 3')
         print('i = 0')
         print('y = 0')
-        print('while i < x:')
+        print('while i <= x:')
         print('\ti = i + 1')
         print('\ty = y + i')
         print('print(y)')
@@ -242,6 +228,20 @@ class PythonTest:
     def test_20(self):
         print('--------------------------------------------')
         print('Frage 20:')
+        print('x = 3')
+        print('i = 0')
+        print('y = 0')
+        print('while i < x:')
+        print('\ti = i + 1')
+        print('\ty = y + i')
+        print('print(y)')
+        answer = None
+        while answer != self.sol['20']:
+            answer = input('Was gibt der print Befehl aus?')
+
+    def test_21(self):
+        print('--------------------------------------------')
+        print('Frage 21:')
         print('def return_second(my_list):')
         print('\tsecond_item = my_list[x]')
         print('\treturn second_item\n')
@@ -249,23 +249,23 @@ class PythonTest:
         print('print(return_second(a_list))\n')
         print('7')
         answer = None
-        while answer != self.sol['20']:
+        while answer != self.sol['21']:
             answer = input('Was muss x sein, der oben stehende Code stimmt?')
 
-    def test_21(self):
+    def test_22(self):
         print('--------------------------------------------')
-        print('Frage 21:')
+        print('Frage 22:')
         print('def append_str(string, character=" "):')
         print('\tnew_string = string + character\n')
         print('a = "bob"')
         print('a = append_str(a, "fahren")')
         answer = None
-        while answer != self.sol['21']:
-            answer = input('Was steht in a?')
+        while answer != self.sol['22']:
+            answer = input('Was ist a?')
 
-    def test_22(self):
+    def test_23(self):
         print('--------------------------------------------')
-        print('Frage 22:')
+        print('Frage 23:')
         print('def append_str(string, character=" "):')
         print('\tnew_string = string + character')
         print('\treturn new_string\n')
@@ -273,29 +273,17 @@ class PythonTest:
         print('a = append_str(a)')
         print('a = append_str(a, "fahren")')
         answer = None
-        while answer != self.sol['21']:
+        while answer != self.sol['23']:
             answer = input('Was steht in a?') 
 
-    def test_23(self):
+    def test_24(self):
         print('--------------------------------------------')
-        print('Frage 23:')
+        print('Frage 24:')
         print('def my_func(my_list):')
         print('\tmy_list.append(42)\n')
         print('other_list = []')
         print('my_func(other_list)')
         print('print(other_list)')
-        answer = None
-        while answer != self.sol['23']:
-            answer = input('Was gibt der print-Befehl aus?')
-
-    def test_24(self):
-        print('--------------------------------------------')
-        print('Frage 24:')
-        print('def my_func(my_int):')
-        print('\tmy_int = my_int + 1\n')
-        print('other_int = 3')
-        print('my_func(other_int)')
-        print('print(other_int)')
         answer = None
         while answer != self.sol['24']:
             answer = input('Was gibt der print-Befehl aus?')
@@ -305,13 +293,74 @@ class PythonTest:
         print('Frage 25:')
         print('def my_func(my_int):')
         print('\tmy_int = my_int + 1\n')
+        print('other_int = 3')
+        print('my_func(other_int)')
+        print('print(other_int)')
+        answer = None
+        while answer != self.sol['25']:
+            answer = input('Was gibt der print-Befehl aus?')
+
+    def test_26(self):
+        print('--------------------------------------------')
+        print('Frage 26:')
+        print('def my_func(my_int):')
+        print('\tmy_int = my_int + 1\n')
         print('\treturn my_int\n')
         print('other_int = 3')
         print('other_int = my_func(other_int)')
         print('print(other_int)')
         answer = None
-        while answer != self.sol['25']:
+        while answer != self.sol['26']:
             answer = input('Was gibt der print-Befehl aus?')
+
+    def test_27(self):
+        print('--------------------------------------------')
+        print('Frage 27:')
+        print('my_dict = {"foo":"bar",\n "bar":"foot"}')
+        print('z = my_dict["foo"]')
+        answer = None
+        while answer != self.sol['27']:
+            answer = input('Was ist z?')
+
+    def test_28(self):
+        print('--------------------------------------------')
+        print('Frage 28:')
+        print('my_dict = {"foo":"bar",\n "bar":"foot"}')
+        print('z = my_dict[0]')
+        answer = None
+        while answer != self.sol['28']:
+            answer = input('Gibt dieser Code einen Fehler? Antworte Ja oder Nein.')
+
+    def test_29(self):
+        print('--------------------------------------------')
+        print('Frage 29:')
+        print('my_dict = {"foo":"bar",\n "bar":"foot"}')
+        print('z = "foo" in my_dict')
+        answer = None
+        while answer != self.sol['29']:
+            answer = input('Was ist z?')
+
+    def test_30(self):
+        print('--------------------------------------------')
+        print('Frage 30:')
+        print('my_dict = {"foo":"bar",\n "bar":"foot"}')
+        print('z = "foo" in my_dict.values()')
+        answer = None
+        while answer != self.sol['30']:
+            answer = input('Was ist z?')
+
+    def test_31(self):
+        print('--------------------------------------------')
+        print('Frage 31:')
+        print('my_tup = (1., 3, [4])')
+        print('my_tup[2].append(2)')
+        print('item3 = my_tup[2]')
+        answer = None
+        while answer != self.sol['31']:
+            answer = input('Was ist item3?')
+
+
+
 
 
 
